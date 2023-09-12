754c785ca63513f91f44130ec8995b962228a09d
rorymccann95 <rorymccann95@users.noreply.github.com>
2023-09-12 13:55:59 +0100

    Created new UI using WPF

--- WPFUI/MainWindow.xaml.cs
+++ WPFUI/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WPFUI
         {
             InitializeComponent();
         }
+
+        private void createEmployeeIdButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
