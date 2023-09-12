382dc8886850b85bebbc3ad53a8f5dfd362be296
rorymccann95 <rorymccann95@users.noreply.github.com>
2023-09-12 13:35:58 +0100

    step 1 encapsulate functionality in a new method

--- WinFormUI/Dashboard.cs
+++ WinFormUI/Dashboard.cs
@@ -19,7 +19,12 @@ namespace WinFormUI
 
         private void generateEmployeeIdButton_Click(object sender, EventArgs e)
         {
-            string employeeId = $@"{ firstNameText.Text.Substring(0, 4) }{ lastNameText.Text.Substring(0, 4) }{ DateTime.Now.Millisecond }";
+            GenerateEmployeeID();
+        }
+
+        private void GenerateEmployeeID()
+        {
+            string employeeId = $@"{firstNameText.Text.Substring(0, 4)}{lastNameText.Text.Substring(0, 4)}{DateTime.Now.Millisecond}";
             employeeIdText.Text = employeeId;
         }
     }
